5b36feb8ff5ae2177f3bf393b01ae1b973d29bf8
1znaiv4 <dreamzeroseven@gmail.com>
2024-11-26 19:03:23 +0800

    Update everest.sh

--- everest.sh
+++ everest.sh
@@ -1,26 +1,11 @@
-rm -rf .repo/local_manifests/
-repo init -u https://github.com/EverestOS-AOSP/manifest -b 15 --git-lfs
+rm -rf .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/EverestOS-AOSP/manifest -b 15 --git-lfs -g default,-mips,-darwin,-notdefault
+
+git clone https://github.com/1znaiv4/local_manifest.git --depth 1 -b everest .repo/local_manifests
+
 /opt/crave/resync.sh
-
-rm -rf device/xiaomi/mojito
-rm -rf device/xiaomi/sm6150-common
-rm -rf kernel/xiaomi/mojito
-rm -rf vendor/xiaomi/sm6150-common
-rm -rf hardware/xiaomi
-
-git clone https://github.com/1znaiv4/android_device_xiaomi_mojito.git --depth 1 -b 15 device/xiaomi/mojito
-git clone https://github.com/1znaiv4/android_device_xiaomi_sm6150-common.git --depth 1 -b 15 device/xiaomi/sm6150-common
-
-git clone https://github.com/ProjectEverest-Devices/android_kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
-
-git clone https://gitlab.com/bliss-mojito/android_vendor_xiaomi_mojito.git --depth 1 -b 14 vendor/xiaomi/mojito
-git clone https://gitlab.com/bliss-mojito/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 14 vendor/xiaomi/sm6150-common
-
-git clone https://github.com/ProjectEverest-Devices/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
-
-# signing key for everestos
-git clone https://github.com/mojito-keys/vendor_lineage_signing.git --depth 1 -b main vendor/lineage
 
 . build/envsetup.sh
 lunch everest_mojito-ap3a-user
-mka everest -j$(nproc --all)
+make installclean
+make everest
